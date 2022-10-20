1e6afafd55d04e28ff51d73055995b163992f124
Librariann <okpc0306@naver.com>
2022-10-20 15:47:29 +0900

    [edit] db,init,pug,webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -1,64 +1,63 @@
 const path = require("path");
-const autoprefixer = require("autoprefixer");
-const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 
 const MODE = process.env.WEBPACK_ENV;
 const ENTRY_FILE = path.resolve(__dirname, "assets", "js", "main.js");
 const OUTPUT_DIR = path.join(__dirname, "static");
 
 const config = {
-    entry: ["@babel/polyfill", ENTRY_FILE],
-    mode: MODE,
-    module: {
+  entry: ["@babel/polyfill", ENTRY_FILE],
+  mode: MODE,
+  module: {
     rules: [
-        {
-            test: /\.(js)$/,
-            use: [
-                {
-                    loader:'babel-loader'
-                }
-            ]
-        },
-    {
+      {
+        test: /\.(js)$/,
+        use: [
+          {
+            loader: "babel-loader",
+          },
+        ],
+      },
+      {
         test: /\.(scss)$/,
         use: [
-            {
-                loader: MiniCssExtractPlugin.loader
+          {
+            loader: MiniCssExtractPlugin.loader,
+          },
+          {
+            loader: "css-loader",
+          },
+          {
+            loader: "postcss-loader",
+            options: {
+              postcssOptions: {
+                plugins: [
+                  [
+                    "autoprefixer",
+                    {
+                      overrideBrowserslist: "cover 99.5%",
+                    },
+                  ],
+                ],
+              },
             },
-            {
-                loader: "css-loader"
-            },
-            {
-                loader: "postcss-loader",
-                options: {
-                    postcssOptions: {
-                        plugins: [
-                            [
-                                'autoprefixer',
-                                {
-                                    overrideBrowserslist: "cover 99.5%"
-                                },
-                            ]
-                        ]
-                    }
-                }           
-            },
-            {
-                loader: "sass-loader"
-            }
-        ]
-    }   
-    ]
-},
-    output: {
-        path: OUTPUT_DIR,
-        filename: "[name].js"
-    },
-plugins: [
-        new MiniCssExtractPlugin({
-            filename: 'styles.css' 
-        }),
-    ]
+          },
+          {
+            loader: "sass-loader",
+          },
+        ],
+      },
+    ],
+  },
+  output: {
+    path: OUTPUT_DIR,
+    filename: "[name].js",
+  },
+  plugins: [
+    new MiniCssExtractPlugin({
+      filename: "styles.css",
+    }),
+  ],
 };
 
 module.exports = config;
